0af5aa50d9c0a167ac20e6b3b14d837a2ec4a311
Abhishek Velichala <70202321+AbhishekRaoV@users.noreply.github.com>
2025-04-22 16:52:27 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -10,58 +10,62 @@ pipeline{
         stage("Setup Environment"){
             steps{
                 script{
-                    ws('dataops'){
+                    
                         sh '''
-                        source venv/bin/activate
-                        pip3 install -r requirements.txt
+                            python3 -m venv venv
+                            . venv/bin/activate
+                            sudo pip3 install -r requirements.txt --break-system-packages
                         '''
                     }
                 }
             }
-        }
+        
         stage("Data Ingestion"){
             steps{
                 script{
-                    ws('dataops'){
+                    
                         sh "python3 data_ingestion.py"
                     }
                 }
-            }
+            
         }
         stage("Data Processing"){
             steps{
                 script{
-                    ws('dataops'){
+                    
                         sh "python3 data_processing.py"
                     }
-                }
+                
             }
         }
         stage("Data Validation"){
             steps{
                 script{
-                    ws('dataops'){
+                    
                         sh "python3 data_validation.py"
                     }
-                }
+                
             }
         }
         stage("Load to database"){
             steps{
                 script{
-                    ws('dataops'){
+                    
                         sh "python3 load_to_database.py"
                     }
-                }
+                
             }
         }
         stage("Report Generation"){
             steps{
                 script{
-                    ws('dataops'){
+                    
                         sh "python3 generate_report.py"
+                        archiveArtifacts artifacts: 'reports/data_insights_report.html', followSymlinks: false
+                        archiveArtifacts artifacts: 'reports/correlation_plot.png', followSymlinks: false
+                        archiveArtifacts artifacts: 'reports/cluster_plot.png', followSymlinks: false
                     }
-                }
+                
             }
         }
     }
